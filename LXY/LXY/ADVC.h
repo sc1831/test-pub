31e4fec5bb6c4c84a9368097abfea266e5727306
guohui <guohui@25677754-c2c8-446c-8108-d43b6eb462ed>
2016-05-12 02:13:42 +0000

    广告
    git-svn-id: svn://221.204.177.11/lexianyu_ios@163 25677754-c2c8-446c-8108-d43b6eb462ed

---
+++ LXY/LXY/ADVC.h
@@ -0,0 +1,13 @@
+//
+//  ADVC.h
+//  LXY
+//
+//  Created by guohui on 16/5/12.
+//  Copyright © 2016年 guohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADVC : UIViewController
+
+@end
